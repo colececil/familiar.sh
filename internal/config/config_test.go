b8500aeed48114a997402edd99b44b3e6bc427b5
Cole Cecil <cole.cecil@gmail.com>
2023-11-23 22:38:35 -0600

    Finish writing unit tests for Config

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/colececil/familiar.sh/internal/test"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"gopkg.in/yaml.v3"
 )
 
 var _ = Describe("Config", func() {
@@ -173,11 +174,180 @@ var _ = Describe("Config", func() {
 	})
 
 	Describe("UpdatePackage", func() {
+		const packageStartingVersionString = "1.0.0"
+		const packageUpdateVersionString = "1.0.1"
+
+		var packageManager1 *ConfiguredPackageManager
+		var packageManager2 *ConfiguredPackageManager
+
+		BeforeEach(func() {
+			_ = config.AddPackageManager(packageManager1Name, packageManagerRegistry)
+			_ = config.AddPackage(packageManager1Name, package1Name,
+				packagemanagers.NewVersion(packageStartingVersionString))
+			_ = config.AddPackage(packageManager1Name, package2Name,
+				packagemanagers.NewVersion(packageStartingVersionString))
+
+			_ = config.AddPackageManager(packageManager2Name, packageManagerRegistry)
+			_ = config.AddPackage(packageManager2Name, package1Name,
+				packagemanagers.NewVersion(packageStartingVersionString))
+			_ = config.AddPackage(packageManager2Name, package2Name,
+				packagemanagers.NewVersion(packageStartingVersionString))
+
+			packageManager1 = &config.PackageManagers[0]
+			packageManager2 = &config.PackageManagers[1]
+		})
+
+		It("should update the given package to the given version under the given package manager", func() {
+			err := config.UpdatePackage(packageManager1Name, package1Name,
+				packagemanagers.NewVersion(packageUpdateVersionString))
+			Expect(err).To(BeNil())
+			Expect(packageManager1.Packages[0].Version).To(Equal(packageUpdateVersionString))
+			Expect(packageManager1.Packages[1].Version).To(Equal(packageStartingVersionString))
+			Expect(packageManager2.Packages[0].Version).To(Equal(packageStartingVersionString))
+			Expect(packageManager2.Packages[1].Version).To(Equal(packageStartingVersionString))
+
+			err = config.UpdatePackage(packageManager2Name, package1Name,
+				packagemanagers.NewVersion(packageUpdateVersionString))
+			Expect(err).To(BeNil())
+			Expect(packageManager1.Packages[0].Version).To(Equal(packageUpdateVersionString))
+			Expect(packageManager1.Packages[1].Version).To(Equal(packageStartingVersionString))
+			Expect(packageManager2.Packages[0].Version).To(Equal(packageUpdateVersionString))
+			Expect(packageManager2.Packages[1].Version).To(Equal(packageStartingVersionString))
+		})
+
+		It("should return an error if the given package manager is not in the config", func() {
+			err := config.UpdatePackage("invalid", package1Name,
+				packagemanagers.NewVersion(packageUpdateVersionString))
+			Expect(err.Error()).To(Equal("package manager not present"))
+		})
+
+		It("should return an error if the given package is not in the config under the given package manager", func() {
+			err := config.UpdatePackage(packageManager1Name, "invalid",
+				packagemanagers.NewVersion(packageUpdateVersionString))
+			Expect(err.Error()).To(Equal("package not present"))
+		})
+
+		It("should return an error if the given package is already set to the given version under the given package "+
+			"manager", func() {
+			err := config.UpdatePackage(packageManager1Name, package1Name,
+				packagemanagers.NewVersion(packageStartingVersionString))
+			Expect(err.Error()).To(Equal("package already set to given version"))
+		})
 	})
 
 	Describe("RemovePackage", func() {
+		var packageManager1 *ConfiguredPackageManager
+		var packageManager2 *ConfiguredPackageManager
+
+		BeforeEach(func() {
+			const packageVersionString = "1.0.0"
+			_ = config.AddPackageManager(packageManager1Name, packageManagerRegistry)
+			_ = config.AddPackage(packageManager1Name, package1Name, packagemanagers.NewVersion(packageVersionString))
+			_ = config.AddPackage(packageManager1Name, package2Name, packagemanagers.NewVersion(packageVersionString))
+
+			_ = config.AddPackageManager(packageManager2Name, packageManagerRegistry)
+			_ = config.AddPackage(packageManager2Name, package3Name, packagemanagers.NewVersion(packageVersionString))
+
+			packageManager1 = &config.PackageManagers[0]
+			packageManager2 = &config.PackageManagers[1]
+		})
+
+		It("should remove the given package from the config under the given package manager", func() {
+			err := config.RemovePackage(packageManager1Name, package1Name)
+			Expect(err).To(BeNil())
+			Expect(len(packageManager1.Packages)).To(Equal(1))
+			Expect(packageManager1.Packages[0].Name).To(Equal(package2Name))
+
+			err = config.RemovePackage(packageManager1Name, package2Name)
+			Expect(err).To(BeNil())
+			Expect(len(packageManager1.Packages)).To(Equal(0))
+		})
+
+		It("should return an error if the given package manager is not in the config", func() {
+			err := config.RemovePackage("invalid", package1Name)
+			Expect(err.Error()).To(Equal("package manager not present"))
+			Expect(len(packageManager1.Packages)).To(Equal(2))
+			Expect(len(packageManager2.Packages)).To(Equal(1))
+		})
+
+		It("should return an error if the given package is not in the config under the given package manager", func() {
+			err := config.RemovePackage(packageManager2Name, package1Name)
+			Expect(err.Error()).To(Equal("package not present"))
+			Expect(len(packageManager1.Packages)).To(Equal(2))
+			Expect(len(packageManager2.Packages)).To(Equal(1))
+		})
 	})
 
 	Describe("YamlString", func() {
+		DescribeTable("should return the correct YAML representation of the config",
+			func(expectedYamlString string, configSetupFunc func()) {
+				configSetupFunc()
+				result, err := config.YamlString()
+				Expect(err).To(BeNil())
+
+				parsedExpected, err := parseYamlString(expectedYamlString)
+				Expect(err).To(BeNil())
+				parsedResult, err := parseYamlString(result)
+				Expect(err).To(BeNil())
+
+				Expect(parsedResult).To(Equal(parsedExpected))
+			},
+			Entry("when the config is empty",
+				`version: 1
+files: []
+scripts: []
+packageManagers: []`,
+				func() {},
+			),
+			Entry("when the config contains package managers with no packages",
+				`version: 1
+files: []
+scripts: []
+packageManagers:
+    - name: packageManager1
+      packages: []
+    - name: packageManager2
+      packages: []`,
+				func() {
+					_ = config.AddPackageManager(packageManager1Name, packageManagerRegistry)
+					_ = config.AddPackageManager(packageManager2Name, packageManagerRegistry)
+				},
+			),
+			Entry("when the config contains package managers with packages",
+				`version: 1
+files: []
+scripts: []
+packageManagers:
+    - name: packageManager1
+      packages:
+          - name: package1
+            version: 1.0.0
+          - name: package2
+            version: 1.1.1
+    - name: packageManager2
+      packages: []
+    - name: packageManager3
+      packages:
+          - name: package3
+            version: 1.0.1`,
+				func() {
+					_ = config.AddPackageManager(packageManager1Name, packageManagerRegistry)
+					_ = config.AddPackage(packageManager1Name, package1Name, packagemanagers.NewVersion("1.0.0"))
+					_ = config.AddPackage(packageManager1Name, package2Name, packagemanagers.NewVersion("1.1.1"))
+
+					_ = config.AddPackageManager(packageManager2Name, packageManagerRegistry)
+
+					_ = config.AddPackageManager(packageManager3Name, packageManagerRegistry)
+					_ = config.AddPackage(packageManager3Name, package3Name, packagemanagers.NewVersion("1.0.1"))
+				},
+			),
+		)
 	})
 })
+
+// parseYamlString parses the given yaml string and returns the result
+func parseYamlString(yamlString string) (interface{}, error) {
+	var result interface{}
+	err := yaml.Unmarshal([]byte(yamlString), &result)
+	return result, err
+}
